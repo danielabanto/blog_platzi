6135de849064e0f02296705ecab44819baba734d
Daniel Abanto <daniel4.ah@gmail.com>
2020-06-25 17:19:48 -0500

    cambios en rutas chunk

---
+++ build/precache-manifest.0c8c5fdcc88f87d8d27d08b7d15985f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03d6e5fa70f7ac0bb143aba04404fc3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3fe2a3b6a4117582f3f8",
+    "url": "/static/css/main.b03c2a85.chunk.css"
+  },
+  {
+    "revision": "a76fc5a5f219271705c9",
+    "url": "/static/js/2.ff61583a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ff61583a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fe2a3b6a4117582f3f8",
+    "url": "/static/js/main.f864dfd6.chunk.js"
+  },
+  {
+    "revision": "bd31cc5442cc3d72c629",
+    "url": "/static/js/runtime-main.5727db3b.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/blog_platzi/precache-manifest.5a2d7fcc39f31d7907e462273a5940e2.js"
+  "/precache-manifest.0c8c5fdcc88f87d8d27d08b7d15985f9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/blog_platzi/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
