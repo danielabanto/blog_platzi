d362b1a2c7df04ac1de8291033e3439982524f49
Daniel Abanto <daniel4.ah@gmail.com>
2020-06-25 16:41:21 -0500

    Cambios necesarios en el manifest.js

---
+++ build/precache-manifest.5a2d7fcc39f31d7907e462273a5940e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cdeb584af655761865cc013bae7d2a8",
+    "url": "/blog_platzi/index.html"
+  },
+  {
+    "revision": "3fe2a3b6a4117582f3f8",
+    "url": "/blog_platzi/static/css/main.b03c2a85.chunk.css"
+  },
+  {
+    "revision": "a76fc5a5f219271705c9",
+    "url": "/blog_platzi/static/js/2.ff61583a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/blog_platzi/static/js/2.ff61583a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fe2a3b6a4117582f3f8",
+    "url": "/blog_platzi/static/js/main.f864dfd6.chunk.js"
+  },
+  {
+    "revision": "4ea86ccb8b0e277abd47",
+    "url": "/blog_platzi/static/js/runtime-main.f9c0f1ee.js"
+  }
+]);
